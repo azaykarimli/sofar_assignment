70e711d39b2836f231ca56d83ffd25fa1653d3e3
aayush11101998 <5160727@studenti.unige.it>
2022-09-02 12:31:52 +0200

    updts

--- src/webots_ros2_tiago/launch/multirobot_launch.py
+++ src/webots_ros2_tiago/launch/multirobot_launch.py
@@ -15,33 +15,70 @@
 # limitations under the License.
 
 """Launch Webots and the controllers."""
-
+import sys
 import os
 import pathlib
 import launch
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_packages_with_prefixes
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 
 
-
-PACKAGE_NAME = 'webots_ros2_universal_robot'
+PACKAGE_NAME = 'webots_ros2_tiago'
 
 
 def generate_launch_description():
+    optional_nodes = []
     package_dir = get_package_share_directory(PACKAGE_NAME)
     tiago_iron_description = pathlib.Path(os.path.join(package_dir, 'resource', 'tiago_webots.urdf')).read_text()
-    tiago_iron_description = pathlib.Path(os.path.join(package_dir, 'resource', 'tiago_webots.urdf')).read_text()
+    tiago_iron1_description = pathlib.Path(os.path.join(package_dir, 'resource', 'tiago_webots.urdf')).read_text()
     tiago_iron_control_params = os.path.join(package_dir, 'resource', 'ros2_control.yml')
-    tiago_iron_control_params = os.path.join(package_dir, 'resource', 'ros2_control1.yml')
-
+    tiago_iron1_control_params = os.path.join(package_dir, 'resource', 'ros2_control1.yml')
+    world = LaunchConfiguration('world')
+    mode = LaunchConfiguration('mode')
+    use_rviz = LaunchConfiguration('rviz', default=True)
+    use_nav = LaunchConfiguration('nav', default=True)
+    use_slam = LaunchConfiguration('slam', default=True)
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    nav2_map = os.path.join(package_dir, 'resource', 'map.yaml')
     # Webots
     webots = WebotsLauncher(world=os.path.join(package_dir, 'worlds', 'default.wbt'))
 
     # Driver nodes
     # When having multiple robot it is enough to specify the `additional_env` argument.
     # The `WEBOTS_ROBOT_NAME` has to match the robot name in the world file.
+    controller_manager_timeout = ['--controller-manager-timeout', '50']
+    controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
+
+    use_deprecated_spawner_py = 'ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'galactic'
+
+    diffdrive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['diffdrive_controller'] + controller_manager_timeout,
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+    )
     
+    mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel')]
+    if 'ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] in ['humble', 'rolling']:
+        mappings.append(('/diffdrive_controller/odom', '/odom'))
+
     tiago_iron_driver = Node(
         package='webots_ros2_driver',
         executable='driver',
@@ -49,64 +86,77 @@ def generate_launch_description():
         additional_env={'WEBOTS_ROBOT_NAME': 'tiago iron'},
         namespace='tiago_iron',
         parameters=[
-            {'robot_description': tiagobot_description},
+            {'robot_description': tiago_iron_description},
             {'use_sim_time': True},
             tiago_iron_control_params
         ]
     )
-    tiago_iron(1)_driver = Node(
+    tiago_iron1_driver = Node(
         package='webots_ros2_driver',
         executable='driver',
         output='screen',
         additional_env={'WEBOTS_ROBOT_NAME': 'tiago iron(1)'},
         namespace='tiago_iron(1)',
         parameters=[
-            {'robot_description': tiagobot_description},
+            {'robot_description': tiago_iron1_description},
             {'use_sim_time': True},
-            tiago_iron(1)_control_params
+            tiago_iron1_control_params
         ]
     )
 
-    controller_manager_timeout = ['--controller-manager-timeout', '75']
-    controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
-    use_deprecated_spawner_py = 'ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'galactic'
-    ur5e_trajectory_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
         output='screen',
-        prefix=controller_manager_prefix,
-        arguments=['ur_joint_trajectory_controller', '-c', 'ur5e/controller_manager'] + controller_manager_timeout,
+        parameters=[{
+           'robot_description': '<robot name=""><link name=""/></robot>'
+        }],
     )
-    ur5e_joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
+
+    footprint_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
         output='screen',
-        prefix=controller_manager_prefix,
-        arguments=['ur_joint_state_broadcaster', '-c', 'ur5e/controller_manager'] + controller_manager_timeout,
+        arguments=['1', '0', '0', '1', '0', '0', 'base_link', 'base_footprint'],
     )
-    abb_trajectory_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
+
+    rviz_config = os.path.join(get_package_share_directory('webots_ros2_tiago'), 'resource', 'default.rviz')
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
         output='screen',
-        prefix=controller_manager_prefix,
-        arguments=['abb_joint_trajectory_controller', '-c', 'abb/controller_manager'] + controller_manager_timeout,
+        arguments=['--display-config=' + rviz_config],
+        parameters=[{'use_sim_time': use_sim_time}],
+        condition=launch.conditions.IfCondition(use_rviz)
     )
-    abb_joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner' if not use_deprecated_spawner_py else 'spawner.py',
+
+    if 'nav2_bringup' in get_packages_with_prefixes():
+        optional_nodes.append(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')),
+            launch_arguments=[
+                ('map', nav2_map),
+                ('use_sim_time', use_sim_time),
+            ],
+            condition=launch.conditions.IfCondition(use_nav)))
+
+    slam_toolbox = Node(
+        parameters=[{'use_sim_time': use_sim_time}],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
         output='screen',
-        prefix=controller_manager_prefix,
-        arguments=['abb_joint_state_broadcaster', '-c', 'abb/controller_manager'] + controller_manager_timeout,
+        condition=launch.conditions.IfCondition(use_slam)
     )
 
     # Control nodes
-    tiago_controller = Node(
+    tiagobot = Node(
         package=PACKAGE_NAME,
         executable='tiago_iron',
         namespace='tiago_iron',
         output='screen'
     )
-    tiago1_controller = Node(
+    tiagobot1 = Node(
         package=PACKAGE_NAME,
         executable='tiago_iron1',
         namespace='tiago_iron(1)',
@@ -114,15 +164,28 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='default.wbt',
+            description='Choose one of the world files from `/webots_ros2_tiago/world` directory'
+        ),
+        DeclareLaunchArgument(
+            'mode',
+            default_value='realtime',
+            description='Webots startup mode'
+        ),
+        joint_state_broadcaster_spawner,
+        diffdrive_controller_spawner,
+        rviz,
+        robot_state_publisher,
+        slam_toolbox ,
+        footprint_publisher,
         webots,
-        tiago1_controller,
-        tiago_controller,
+        tiagobot,
+        tiagobot1,
         tiago_iron_driver,
-        tiago_iron(1)_driver,
-        ur5e_trajectory_controller_spawner,
-        ur5e_joint_state_broadcaster_spawner,
-        abb_trajectory_controller_spawner,
-        abb_joint_state_broadcaster_spawner,
+        tiago_iron1_driver,
+        
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
